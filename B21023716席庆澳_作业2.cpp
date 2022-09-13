afd9e3946cc2760dc0cbdb251329e82e4bc0fd8a
江泽杉 <86450354+ANTONYGitH@users.noreply.github.com>
2022-09-13 11:23:23 +0800

    Add files via upload

---
+++ B21023716席庆澳_作业2.cpp
@@ -0,0 +1,18 @@
+﻿//作业2 配置好OpenCV，随便显示一幅图像。附上代码和显示结果截图
+//#include <opencv2/core.hpp>
+//#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+
+int main()
+{
+    Mat img = imread("B21023716席庆澳_作业2.jpg");
+    namedWindow("B21023716席庆澳",0);
+    resizeWindow("B21023716席庆澳",800,540);
+    imshow("B21023716席庆澳", img);
+    waitKey(1000);//1s,   waitKey(0);写0的话会窗口不会自己关闭，需要按任意键关闭
+    destroyAllWindows();
+    return 0;
+}
+//姓名：席庆澳|学号：B21023716|完成时间：2022年8月31日11:20:07
